f9c6902ca8de572cfcb1536bb4b0454e150a8689
Bruno Michel <bmichel@menfin.info>
2015-02-19 18:45:51 +0100

    Update dependencies

--- src/ast.js
+++ src/ast.js
@@ -1,24 +1,7 @@
 var cmark = require("commonmark");
 
 exports.parseAST = function(markdown) {
-  var transform = function(items) {
-    for (let item of items) {
-      if (item.children) {
-        transform(item.children);
-      }
-
-      // Split lines of code for fenced code, to improve diffs
-      if (item.t === "FencedCode") {
-        item.inline_content = item.string_content.split("\n")
-          .slice(0, -1)
-          .map(l => ({ t: "CodeLine", c: l }));
-        item.string_content = null;
-      }
-    }
-  };
-
-  let reader = new cmark.DocParser();
-  let ast = reader.parse(markdown);
-  transform([ast]);
-  return ast;
+  let reader = new cmark.Parser();
+  let parsed = reader.parse(markdown);
+  return parsed;
 };
